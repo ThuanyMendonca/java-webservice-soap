939acd895ecaae45095ea174e30de34f11acfaf1
Thuany Mendonca <thuany1997@hotmail.com>
2025-10-07 18:06:50 -0300

    adicionando o projeto basico

---
+++ src/main/java/com/github/soap/webservice/customers_adminstration/CustomersAdminstrationApplication.java
@@ -0,0 +1,13 @@
+package com.github.soap.webservice.customers_adminstration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomersAdminstrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomersAdminstrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/soap/webservice/customers_adminstration/CustomersAdminstrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.soap.webservice.customers_adminstration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomersAdminstrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
